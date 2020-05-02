5ff73e281f297b4ae365aaa3391bad2e05aeecd7
Georges Nomicos <georgesnomicos@gmail.com>
2020-05-02 12:21:31 +0200

    created products table

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -13,7 +13,15 @@ CREATE TABLE IF NOT EXISTS users (
   username TEXT UNIQUE NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS products (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  product_name TEXT,
+  product_image_loc TEXT
+);
+
 INSERT OR IGNORE INTO store (id, image) VALUES (1, '');
 INSERT OR IGNORE INTO store (id, image) VALUES (2, '');
 
 INSERT OR IGNORE INTO camera1 (id, image_capture) VALUES (1,0);
+
+INSERT INTO products (product_name, product_image_loc) VALUES ("Beurre president", "lemon.jpg");
