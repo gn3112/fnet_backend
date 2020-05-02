545fd34aa4af334625fecdb7fb5f1604c400ecc2
Georges Nomicos <georgesnomicos@gmail.com>
2020-05-02 12:18:14 +0200

    dynamically create log table for registered user

--- flaskr/login_page.py
+++ flaskr/login_page.py
@@ -40,7 +40,19 @@ def register():
         if not username_exist:
             result = db.execute("INSERT INTO users (username) VALUES ('{}')".format(username))
             db.commit()
+            create_table_log_user(username, db)
             info = "Succesfully registered"
             # return redirect(url_for("login_page.signing_in"))
 
     return render_template('login_page/register.html', exist=username_exist, text_info=info)
+
+def create_table_log_user(username, db):
+    create_table_sql = """ CREATE TABLE IF NOT EXISTS log_{} (
+                                id integer PRIMARY KEY,
+                                product_id integer,
+                                qty integer NOT NULL,
+                                date_added text
+                        );""".format(username)
+    
+    db.execute(create_table_sql)
+    db.commit()
